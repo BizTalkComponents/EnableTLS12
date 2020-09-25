f362dc44f34103d10f6216a2a374004260b336dc
Wael Alissa <wael.alissa@outlook.com>
2020-09-25 09:37:53 +0200

    Initial Upload

---
+++ Src/EnableTLS12EndPointBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BizTalkComponents.WCFCustomExtensions.EnableTLS12Behaviour
+{
+
+    public class EnableTLS12EndPointBehavior : IEndpointBehavior
+    {
+        public void AddBindingParameters(ServiceEndpoint endpoint, BindingParameterCollection bindingParameters)
+        {
+        }
+
+        public void ApplyClientBehavior(ServiceEndpoint endpoint, ClientRuntime clientRuntime)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+        }
+
+        public void ApplyDispatchBehavior(ServiceEndpoint endpoint, EndpointDispatcher endpointDispatcher)
+        { 
+        }
+
+        public void Validate(ServiceEndpoint endpoint) 
+        {  
+        }
+    }
+
+}
+
